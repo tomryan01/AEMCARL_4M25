5018c6e114d4e487be102a6868bcb03b45a1969b
Imane Iraoui <iraoui.imane@gmail.com>
2023-03-11 15:19:28 +0000

    CNN fixed

--- crowd_sim/envs/CNN.py
+++ crowd_sim/envs/CNN.py
@@ -8,19 +8,18 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.input_shape = input_shape
         
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=3, stride=2, padding=1)
         self.relu1 = nn.LeakyReLU(inplace=True)
         self.bn1 = nn.BatchNorm2d(32)
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
         self.relu2 = nn.LeakyReLU(inplace=True)
         self.bn2 = nn.BatchNorm2d(64)
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(64 * (self.input_shape[0] // 4) * 112, 960)
-        
+        self.fc = nn.Linear(28672, 960)
+            
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu1(x)
@@ -30,12 +29,12 @@ class CNN(nn.Module):
         x = self.conv2(x)
         x = self.relu2(x)
         x = self.bn2(x)
-        x = self.maxpool2(x)
         
         x = self.flatten(x)
         x = self.fc(x)
         
         return x.squeeze(1)
+
 
 
 def run (image_np):  
