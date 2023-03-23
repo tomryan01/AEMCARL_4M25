487dc5cd22575a262278beeae1d7d7301d00ada6
tom <tomryan01@outlook.com>
2023-03-23 14:39:03 +0000

    updates to lidar functions

--- crowd_sim/envs/utils/lidar.py
+++ crowd_sim/envs/utils/lidar.py
@@ -2,12 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def scan_lidar(self):
+def scan_lidar(robot, humans, res):
     # get scan as a dictionary {angle_index : distance}
-    res = self.scan_points
     full_scan = {}
-    for h in self.humans:
-        scan = h.get_scan(res, self.robot.px, self.robot.py)
+    for h in humans:
+        scan = h.get_scan(res, robot.px, robot.py)
         for angle in scan:
             if scan[angle] < full_scan.get(angle, np.inf):
                 full_scan[angle] = scan[angle]
@@ -19,24 +18,23 @@ def scan_lidar(self):
     return out_scan
 
 
-def scan_to_points(self, scan):
+def scan_to_points(scan, robot):
     coords = []
     for i in range(len(scan)):
         if scan[i] != np.inf:
             coords.append(
-                [self.robot.px + scan[i] * np.cos(np.deg2rad(i)), self.robot.py + scan[i] * np.sin(np.deg2rad(i))])
+                [robot.px + scan[i] * np.cos(np.deg2rad(i)), robot.py + scan[i] * np.sin(np.deg2rad(i))])
 
     return coords
 
 
-def shift_scan (self, scan, time_step):
+def shift_scan(robot, prev_angle, scan, time_step):
 
-    delta_x = self.robot.vx * time_step
-    delta_y = self.robot.vy * time_step
+    delta_x = robot.vx * time_step
+    delta_y = robot.vy * time_step
     heading_angle = np.atan2(delta_y, delta_x)
 
-    rotation = heading_angle - self.previous_angle
-    self.previous_angle = heading_angle
+    rotation = heading_angle - prev_angle
     shifted = scan + rotation/(2*np.pi)
 
     return shifted
