3b97d9b6e4b64d91153b8c41854cdced60d66ce1
tom <tomryan01@outlook.com>
2023-03-24 15:35:43 +0000

    add LiDAR and JointLidar states

--- crowd_sim/envs/utils/state.py
+++ crowd_sim/envs/utils/state.py
@@ -14,6 +14,9 @@ class FullState(object):
         self.goal_position = (self.gx, self.gy)
         self.velocity = (self.vx, self.vy)
 
+    def __iter__(self):
+        return iter([self.px, self.py, self.vx, self.vy, self.radius, self.gx, self.gy, self.v_pref, self.theta])
+
     def __add__(self, other):
         return other + (self.px, self.py, self.vx, self.vy, self.radius, self.gx, self.gy, self.v_pref, self.theta)
 
@@ -40,6 +43,19 @@ class ObservableState(object):
         return ' '.join([str(x) for x in [self.px, self.py, self.vx, self.vy, self.radius]])
 
 
+class LidarState(object):
+    def __init__(self, res, readings):
+        assert res == len(readings)
+        self.res = res
+        self.readings = readings
+
+    def __add__(self, other):
+        return other + self.readings
+
+    def __str__(self, other):
+        return "Lidar readings with resolution {}".format(self.res)
+
+
 class JointState(object):
     def __init__(self, self_state, human_states):
         assert isinstance(self_state, FullState)
@@ -48,3 +64,12 @@ class JointState(object):
 
         self.self_state = self_state
         self.human_states = human_states
+
+
+class JointStateLidar(object):
+    def __init__(self, self_state, lidar_state):
+        assert isinstance(self_state, FullState)
+        assert isinstance(lidar_state, LidarState)
+
+        self.self_state = self_state
+        self.lidar_state = lidar_state
